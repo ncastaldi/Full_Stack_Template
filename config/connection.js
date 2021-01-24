9f4f57e71b56fd13dfbb6d9e4016972160036e92
Nathan Castaldi <nathan@castaldi.dev>
2021-01-24 10:26:53 -0500

    updated syntax to include JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,29 @@
 // Require external npm packages
 const mysql = require("mysql");
+let connection;
 
-module.exports = {
-    makeConnection: function () {
-        // Declare connection
-        const connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "", // ADD DATABASE PASSWORD
-            database: "your_database_name" // ADD DATABASE NAME
-        });
+// Declare connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "", // ADD DATABASE PASSWORD
+        database: "" // ADD DATABASE NAME
+    });
+};
 
-        // Make connection
-        connection.connect(function (err) {
-            if (err) {
-                console.error("error connecting: " + err.stack);
-                return;
-            }
-
-            console.log("Database connected as id: " + connection.threadId);
-        });
+// Make connection
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
     }
-}
+
+    console.log("Database connected as id: " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
