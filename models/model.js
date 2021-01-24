40c7fc91c200f8b4631cc66f507b10abbf7108e5
Nathan Castaldi <nathan@castaldi.dev>
2021-01-24 10:47:27 -0500

    saved with template data; left comments

--- models/model.js
+++ models/model.js
@@ -1,2 +1,26 @@
 // Require local depenancies
 const orm = require("../config/orm");
+
+// Update 'table_name' on Lines 8, 14, and 19.
+
+let model = {
+    all: function (cb) {
+        orm.all("table_name", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.create("table_name", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("table_name", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller
+module.exports = model;
